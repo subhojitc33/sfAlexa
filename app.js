12ab962d2550cd2ff79ceb3d8f01f93d5bbba937
subhojitc33 <subhojitc3@gmail.com>
2017-04-15 15:29:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ function PleaseWait(req,res,intent) {
 
 function GetCurrentCase(req,res,intent) {
 	org.authenticate({ username: 'subhojit_dev3@gmail.com', password: 'November@2013XD1K3tYl2sQ318v5FhDu6huso'}, function(err, resp){
+	console.log('####'+resp);
 	org.apexRest({oauth:intent.oauth, uri:'EchoCaseControl'},
 		function(err,result) {
 			if(err) {
